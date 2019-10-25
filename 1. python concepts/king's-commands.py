3120571905df20d66c6ac669b79c9fdfae11ed4f
Amirhossein Mahdinejad <TheMn@users.noreply.github.com>
2019-10-25 16:41:12 +0330

    practice with if else statements

---
+++ 1. python concepts/king's-commands.py
@@ -0,0 +1,30 @@
+inp = input().split()
+tag = inp[0]
+command = inp[1]
+sum = 0
+if tag == "food":
+    if command == "water":
+        sum += 0.5
+    elif command == "dinner":
+        sum += 1.0
+    else:
+        sum += 10.0
+elif tag == "promote":
+    if command == "judge":
+        sum += 50.0
+    elif command == "minister":
+        sum += 80.0
+    elif command == "governor":
+        sum += 100.0
+    else:
+        sum += 10.0
+elif tag == "travel":
+    if command == "ground":
+        sum += 45.0
+    elif command == "sea":
+        sum += 58.0
+    else:
+        sum += 10.0
+else:
+    sum += 10.0
+print("{result:.1f}".format(result = sum))
