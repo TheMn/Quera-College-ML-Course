99a08201447738b8b97c90788d4e6cfbd2497dba
TheMn <mt.lroc@outlook.com>
2019-10-30 01:26:27 +0330

    email regex completed

--- 1. python concepts/group-registration.py
+++ 1. python concepts/group-registration.py
@@ -1,18 +1,17 @@
 import hashlib
 import re
 
+
 def is_email_valid(email):
-    return re.search(r"sa?", email) is not None
+    return re.fullmatch(r'[0-9a-zA-Z-_]+[@][0-9a-zA-Z]+[.][0-9a-zA-Z][0-9a-zA-Z]?[0-9a-zA-Z]?', email) is not None
 
 
-test_data = ["salam", "sal", "s", "", "sa", "salm"]
-for test in test_data:
-    print("{}: {}".format(test, is_email_valid(test)))
+def hash_password(password):
+    return hashlib.md5(password.encode()).hexdigest()
 
-# def hash_password(password):
-#     return hashlib.md5(password.encode()).hexdigest()
-#
-# def process_data(data):
-#     data = filter(lambda pair: is_email_valid(pair[0]), data)
-#     data = list(map(lambda t: (t[0], hash_password(t[1])), data))
-#     return data
+
+def process_data(data):
+    data = filter(lambda pair: is_email_valid(pair[0]), data)
+    data = list(map(lambda t: (t[0], hash_password(t[1])), data))
+    return data
+
