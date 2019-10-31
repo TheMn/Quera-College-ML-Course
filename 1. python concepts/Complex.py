21f211c610b3c8f4400f1d1624b059946bb8f656
TheMn <mt.lroc@outlook.com>
2019-10-31 21:15:38 +0330

    Complex class and math file created

---
+++ 1. python concepts/Complex.py
@@ -0,0 +1,17 @@
+class Complex:
+
+    def __init__(self, real, img):
+        self._real = real
+        self._img = img
+
+    def set_real(self, real):
+        self._real = float(real)
+
+    def get_real(self):
+        return float(self._real)
+
+    def __str__(self):
+        return "{r:.2f} {s} {i:.2f}i".format(r=self._real, s="+" if self._img >= 0 else "-", i=self._img if self._img >= 0 else -self._img)
+
+c = Complex(2, 0)
+print(c.__str__())
